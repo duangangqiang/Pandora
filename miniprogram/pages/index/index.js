fc95ed82cf96cd0218083c170a43affe6f611763
duangangqiang <gangqiang.duan@idsuipai.com>
2019-02-12 15:19:22 +0800

    删除readme

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,7 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    
   },
 
   onLoad: function() {
