f10bbe86e55b26761071fa6f43eafde46ef6369a
duangangqiang <gangqiang.duan@idsuipai.com>
2019-02-17 12:19:33 +0800

    修改guide图片文件位置，尝试写todos

--- miniprogram/pages/todos/todos.js
+++ miniprogram/pages/todos/todos.js
@@ -1,66 +1,21 @@
-// pages/todos/todos.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    todos: [],
+    index: 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
 
-  },
+  confirmInput(e) {
+    const index = this.data.index + 1;
+    const todos = [...this.data.todos, { index, txt: e.detail.value }];
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.setData({
+      index,
+      todos,
+    });
   },
 });
